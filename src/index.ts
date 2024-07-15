44adbf0b782d48028e654e63bd045ce5e02d3c60
YSawc <niceage6690@gmail.com>
2024-07-15 16:49:02 +0900

    chore: send line notify without gas.

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,38 @@ dotenv.config();
 
 const gasUrl = process.env.GAS_URL ?? "";
 const obniz = new Obniz(process.env.OBNIZ_ID ?? "");
-obniz.onconnect = async function () {
-  const LoundnessSensor = obniz.wired("Grove_LightSensor", {
-    gnd: 4,
-    vcc: 5,
-    signal: 7,
+
+const postLineNotify = async () => {
+  const lineAapiToken = process.env.LINE_API_TOKEN ?? "";
+  const message = "loudness occured";
+  const body: URLSearchParams = new URLSearchParams({
+    message: message,
   });
 
-  const postLineNotify = async () => {
-    const post_url: string = `${gasUrl}?action=postLineNotify`;
-    const result = await fetch(post_url, {
-      method: "POST",
-      body: JSON.stringify({}),
-    });
-    console.log(result);
-  };
+  const res = await fetch("https://notify-api.line.me/api/notify", {
+    method: "post",
+    headers: {
+      Authorization: "Bearer " + lineAapiToken,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body,
+  });
+
+  const json = await res.json();
+  console.log("line notify post result:");
+  console.log(JSON.stringify(json));
+};
+
+obniz.onconnect = async function() {
+  const LoundnessSensor = obniz.wired("Grove_LightSensor", {
+    gnd: 0,
+    vcc: 1,
+    signal: 3,
+  });
+
   const triggerLoundnessSensor = async () => {
     const loundnessValue = await LoundnessSensor.getWait();
-    let loudnessOverValue = 0;
+    let loudnessOverValue: number;
     if (process.env.LOUDNESS_OVER_VALUE != undefined) {
       loudnessOverValue = Number(process.env.LOUDNESS_OVER_VALUE);
     } else {
@@ -30,7 +44,7 @@ obniz.onconnect = async function () {
     if (
       loundnessValue >= loudnessOverValue
     ) {
-      console.log("loundnessValue");
+      console.log("loundnessValue over");
       console.log(loundnessValue);
       postLineNotify();
     }
