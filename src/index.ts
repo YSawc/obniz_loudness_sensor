4ad26b69bbedf9c0f9797b19cc1442c1a49b9caf
YSawc <niceage6690@gmail.com>
2024-09-30 15:46:57 +0900

    update: update posting api to pyum

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,30 @@ import Obniz from "obniz";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const gasUrl = process.env.GAS_URL ?? "";
 const obniz = new Obniz(process.env.OBNIZ_ID ?? "");
 
-const postLineNotify = async () => {
-  const lineAapiToken = process.env.LINE_API_TOKEN ?? "";
-  const message = "loudness occured";
-  const body: URLSearchParams = new URLSearchParams({
-    message: message,
+const postPyumApi = async (loundnessValue: number) => {
+  const shiftDigit = 1;
+  const sendValue = Math.floor(loundnessValue * (10 ** shiftDigit));
+  const body = JSON.stringify({
+    "sensor_id": Number(process.env.SENSOR_ID),
+    "capture_val": sendValue,
+    "shift_digit": shiftDigit,
   });
-
-  const res = await fetch("https://notify-api.line.me/api/notify", {
+  const res = await fetch(`${process.env.PYUM_URL}/api/capture`, {
     method: "post",
     headers: {
-      Authorization: "Bearer " + lineAapiToken,
-      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Type": "application/json",
+      "Client-Secret": `${process.env.PYUM_OAUTH}`,
     },
     body,
   });
-
-  const json = await res.json();
-  console.log("line notify post result:");
-  console.log(JSON.stringify(json));
+  if (!res.ok) {
+    console.log("In postirng pyum api, error occured.");
+  }
 };
 
-obniz.onconnect = async function() {
+obniz.onconnect = async function () {
   const LoundnessSensor = obniz.wired("Grove_LightSensor", {
     gnd: 0,
     vcc: 1,
@@ -35,36 +34,10 @@ obniz.onconnect = async function() {
 
   const triggerLoundnessSensor = async () => {
     const loundnessValue = await LoundnessSensor.getWait();
-    let loudnessOverValue: number;
-    if (process.env.LOUDNESS_OVER_VALUE != undefined) {
-      loudnessOverValue = Number(process.env.LOUDNESS_OVER_VALUE);
-    } else {
-      loudnessOverValue = 5.0;
-    }
-    if (
-      loundnessValue >= loudnessOverValue
-    ) {
-      console.log("loundnessValue over");
-      console.log(loundnessValue);
-      postLineNotify();
-    }
-  };
-  const getData = async (url: string) => {
-    fetch(url, {
-      method: "GET",
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        if (data.data) {
-          triggerLoundnessSensor();
-        }
-      })
-      .catch((error) => console.log(error));
+    await postPyumApi(loundnessValue);
   };
 
   obniz.onloop = async () => {
-    await getData(gasUrl);
+    triggerLoundnessSensor();
   };
 };
